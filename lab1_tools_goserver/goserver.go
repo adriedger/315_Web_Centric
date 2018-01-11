885122471a970ba257b5b30c8ed7dda7d354b443
Andre Driedger <driedgera2@students.cs.macewan.ca>
2018-01-10 19:30:10 -0700

    lab1 stuff

---
+++ lab1_tools_goserver/goserver.go
@@ -0,0 +1,57 @@
+package main
+
+import(
+    "fmt"
+    "net/http"
+    "os"
+    "time"
+    "io"
+    "bytes"
+)
+
+var variable *bytes.Buffer = &bytes.Buffer{}
+
+func handleVariable(w http.ResponseWriter, r *http.Request){
+    switch r.Method{
+    case "GET":
+        _, err := io.Copy(w, variable)
+        if err != nil {
+            http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+            return
+        }
+    case "Post":
+        _, err := io.Copy(w, variable)
+        if err != nil {
+            http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+            return
+        }
+    default:
+            http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+            return
+    }
+}
+
+func handleDate(w http.ResponseWriter, r *http.Request){
+    fmt.Fprintf(w, "%d-%d-%d\n", time.Now().Year(), time.Now().Month(), time.Now().Day())
+}
+
+func handleError(w http.ResponseWriter, r *http.Request){
+    http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
+
+func handler(w http.ResponseWriter, r *http.Request){
+    fmt.Println("Got one!")
+}
+
+func main(){
+    http.HandleFunc("/", handler)
+    http.HandleFunc("/api/v1/error", handleError)
+    http.HandleFunc("/api/v1/date", handleDate)
+    http.HandleFunc("/api/v1/variable", handleVariable)
+
+    err := http.ListenAndServe(":8080", nil)
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "error: %v\n", err)
+        os.Exit(1)
+    }
+}
