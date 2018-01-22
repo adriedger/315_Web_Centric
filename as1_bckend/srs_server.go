0f361ed350182797206013bc21cd6bb22612b7af
Andre Driedger <adriedge@gmail.com>
2018-01-21 21:17:12 -0700

    asign1 updates

--- as1_bckend/srs_server.go
+++ as1_bckend/srs_server.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"encoding/json"
-	//	"fmt"
+	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
 	"sync"
@@ -10,12 +10,48 @@ import (
 
 var mutex sync.Mutex
 
+type User struct {
+	Name string
+	ID   string
+}
+
+type Class struct {
+	Name    string
+	ID      string
+	Creator User
+	//	Joined  []User
+	//	Questions	[]Question
+
+}
+
+func handlePostClass(w http.ResponseWriter, r *http.Request) {
+	var jsonBlob = []byte(`[{"Name": "CMPT 101"}]`)
+	var classes []Class
+	//	class := Class{"", "9000", User{"", "1234"}}
+	err := json.Unmarshal(jsonBlob, &classes)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		fmt.Println("error:", err)
+		return
+	}
+	classes[0].ID = "9000"
+	classes[0].Creator = User{"yo", "1234"}
+	//fmt.Printf("%+v", classes)
+	js, err := json.Marshal(classes[0])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
 func handleGetHome(w http.ResponseWriter, r *http.Request) {
 	type HomeOptions struct {
-		CreateClassURL string
-		JoinClassURL   string
+		CreateClassLink string
+		JoinClassLink   string
 	}
-	home := HomeOptions{"/api/v1/classes/create", "/api/v1/classes/join"}
+	home := HomeOptions{"/api/v1/classes?q=create", "/api/v1/classes?q=join"}
 	js, err := json.Marshal(home)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -28,6 +64,7 @@ func handleGetHome(w http.ResponseWriter, r *http.Request) {
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/api/v1/", handleGetHome).Methods("GET")
+	router.HandleFunc("/api/v1/classes/create/", handlePostClass).Methods("POST")
 
 	http.ListenAndServe(":8080", router)
 }
